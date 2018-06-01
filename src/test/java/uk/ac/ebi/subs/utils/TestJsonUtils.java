741d09891576510c53aa035fc91ec302485ad502
Flávia Penim <fpenim@ebi.ac.uk>
2018-06-01 10:15:13 +0100

    Clean up test data to match validation requirements.

--- src/test/java/uk/ac/ebi/subs/utils/TestJsonUtils.java
+++ src/test/java/uk/ac/ebi/subs/utils/TestJsonUtils.java
@@ -39,7 +39,8 @@ public class TestJsonUtils {
         String template = new String(Files.readAllBytes(Paths.get(file.getPath())));
 
         String json = template.replace(ALIAS, alias);
-        return json.replace(SUBMISSION_URL, submissionUrl);
+        json = json.replace(SUBMISSION_URL, submissionUrl);
+        return json.replace(RELEASE_DATE, LocalDate.now().toString());
     }
 
     public static String getDeleteSampleRelationshipsJson(String submissionUrl, String alias) throws IOException {
@@ -47,7 +48,8 @@ public class TestJsonUtils {
         String template = new String(Files.readAllBytes(Paths.get(file.getPath())));
 
         String json = template.replace(ALIAS, alias);
-        return json.replace(SUBMISSION_URL, submissionUrl);
+        json = json.replace(SUBMISSION_URL, submissionUrl);
+        return json.replace(RELEASE_DATE, LocalDate.now().toString());
     }
 
     public static String getSampleJson(String submissionUrl, String alias) throws IOException {
