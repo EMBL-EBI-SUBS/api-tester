021618f8785c006eec985f3975c22158ee11b36b
kalaij <1823024+kalaij@users.noreply.github.com>
2018-10-05 00:59:26 +0100

    test study creation passes the validation

--- src/test/java/uk/ac/ebi/subs/metabolights/MetaboLightsLcMsSubmission.java
+++ src/test/java/uk/ac/ebi/subs/metabolights/MetaboLightsLcMsSubmission.java
@@ -6,7 +6,8 @@ import uk.ac.ebi.subs.PropertiesManager;
 import uk.ac.ebi.subs.data.objects.ValidationResult;
 import uk.ac.ebi.subs.utils.TestUtils;
 
-import static org.junit.Assert.assertEquals;
+import java.util.Map;
+
 import static uk.ac.ebi.subs.utils.SubmissionOperations.addSample;
 import static uk.ac.ebi.subs.utils.TestUtils.assertNoErrorsInValidationResult;
 
@@ -19,7 +20,6 @@ public class MetaboLightsLcMsSubmission {
     private static final String projectAlias = TestUtils.getRandomAlias();
     private static final String studyAlias = TestUtils.getRandomAlias();
     private static final String sampleAlias = TestUtils.getRandomAlias();
-    private static final String protocolAlias = TestUtils.getRandomAlias();
 
     @BeforeClass
     public static void setUp() throws Exception {
@@ -30,16 +30,10 @@ public class MetaboLightsLcMsSubmission {
 
     @Test
     public void A_addStudy() throws Exception {
-        TestUtils.createMLProtocols(token, "metabolightsProtocols" , submissionUrl, protocolAlias,  pm.getTeamName());
-        String studyUrl = TestUtils.createMLStudy(token, "metabolomicsStudies" , submissionUrl, studyAlias, projectAlias, protocolAlias, pm.getTeamName());
+        Map<String, String> metabolightsProtocolsRefs = TestUtils.createMLProtocols(token, "metabolightsProtocols", submissionUrl, pm.getTeamName());
+        String studyUrl = TestUtils.createMLStudy(token, "metabolomicsStudies" , submissionUrl, studyAlias, projectAlias, metabolightsProtocolsRefs, pm.getTeamName());
         TestUtils.waitForValidationResults(token, studyUrl);
         ValidationResult validationResult = TestUtils.getValidationResultForSubmittable(studyUrl, token);
         assertNoErrorsInValidationResult(validationResult);
     }
-
-    @Test
-    public void B_addSample() throws Exception {
-        addSample(submissionUrl, sampleAlias, token, pm);
-    }
-
 }
